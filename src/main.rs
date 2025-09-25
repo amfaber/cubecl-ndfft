a6ca42d2ff66c789f0a70fb0a0de1ec6fc559ba2
amfaber <andreas.mfaber@gmail.com>
2025-09-25 13:47:29 +0200

    A bit more updates

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,18 @@ fn rmse(a: &[f32], b: &[f32]) -> f32 {
         let d = a[i] - b[i];
         acc += d * d;
     }
+    // acc.sqrt()
     (acc / (a.len() as f32)).sqrt()
+}
+
+fn max_deviation(a: &[f32], b: &[f32]) -> f32 {
+    assert_eq!(a.len(), b.len());
+    let mut acc = 0.;
+    for i in 0..a.len() {
+        let d = (a[i] - b[i]).abs();
+        acc = f32::max(acc, d);
+    }
+    acc
 }
 
 fn fft_2d_cpu(array: &mut Array2<Complex32>, tmp: &mut Array2<Complex32>) {
@@ -74,10 +85,13 @@ fn main() {
 
     let mut in_array = Array2::from_shape_fn((rows, cols as usize), |(i, j)| {
         Complex32 {
-            re: i as f32,
-            im: j as f32,
+            re: rand::random::<f32>(),
+            im: rand::random::<f32>(),
         }
-        // PrettierIndex([i as f32, j as f32])
+        // Complex32 {
+        //     re: i as f32,
+        //     im: j as f32,
+        // }
     });
     // dbg!(&in_array);
     // let host_in = bytemuck::cast_slice::<PrettierIndex, f32>(in_array.as_slice().unwrap()).to_vec();
@@ -137,6 +151,7 @@ fn main() {
     //
     // type RT = cubecl::cuda::CudaRuntime; // adjust runtime if needed
     type RT = cubecl::wgpu::WgpuRuntime; // adjust runtime if needed
+    // type RT = cubecl::cpu::CpuRuntime; // adjust runtime if needed
     let device = <RT as Runtime>::Device::default();
     let client = RT::client(&device);
 
@@ -157,13 +172,14 @@ fn main() {
     let gpu_out = bytemuck::cast_slice::<u8, f32>(&client.read_one(out)).to_vec();
 
     // dbg!(&gpu_out);
-    // let cpu_out = in_array
-    //     .into_iter()
-    //     .flat_map(|complex| [complex.re, complex.im])
-    //     .collect::<Vec<_>>();
+    let cpu_out = in_array
+        .into_iter()
+        .flat_map(|complex| [complex.re, complex.im])
+        .collect::<Vec<_>>();
     // dbg!(&cpu_out);
     // dbg!(&gpu_out);
-    // dbg!(rmse(&cpu_out, &gpu_out));
+    dbg!(max_deviation(&cpu_out, &gpu_out));
+    dbg!(rmse(&cpu_out, &gpu_out));
 
     let timing_method = cubecl::benchmark::TimingMethod::Device;
     let durations = bench.run(timing_method).unwrap();
@@ -174,6 +190,66 @@ fn main() {
     // dbg!(durations);
     // let twiddle_handle = client.create(f32::as_bytes(&tw));
     // Upload
+}
+
+fn print_src(message: &str, src: &ArrayViewMut2<Complex32>) {
+    let prettier = unsafe {
+        std::mem::transmute::<&ArrayViewMut2<Complex32>, &ArrayViewMut2<PrettierIndex>>(src)
+    };
+    println!("{message}\n{:#?}", prettier);
+}
+
+#[derive(bytemuck::Pod, Clone, Copy, Zeroable)]
+#[repr(transparent)]
+struct PrettierIndex([f32; 2]);
+impl std::fmt::Debug for PrettierIndex {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("({}, {})", self.0[0], self.0[1]))
+    }
+}
+
+struct FftBenchmark<R: Runtime> {
+    client: ComputeClient<R::Server, R::Channel>,
+    plan: FftPlan,
+    full_data: Vec<f32>,
+}
+
+impl<R: Runtime> Benchmark for FftBenchmark<R> {
+    type Input = (Handle, Handle);
+
+    type Output = Handle;
+
+    fn prepare(&self) -> Self::Input {
+        let input = self.client.create(f32::as_bytes(&self.full_data));
+        let tmp = self.client.create(f32::as_bytes(&self.full_data));
+        (input, tmp)
+    }
+
+    fn execute(&self, input: Self::Input) -> Result<Self::Output, String> {
+        let (mut input, mut tmp) = input;
+        let output = execute_ops::<R>(
+            input,
+            &mut tmp,
+            &self.plan,
+            Direction::Forward,
+            &self.client,
+            &FftParams {
+                vec_width: 4,
+                local_side_len: 64,
+                threads_per_cube: 256,
+            },
+        );
+
+        Ok(output)
+    }
+
+    fn name(&self) -> String {
+        "Fused fft kernel in local memory".to_string()
+    }
+
+    fn sync(&self) {
+        future::block_on(self.client.sync())
+    }
 }
 
 fn cpu_six_step(array: &Array2<PrettierIndex>) {
@@ -227,208 +303,3 @@ fn cpu_six_step(array: &Array2<PrettierIndex>) {
     // dbg!(&src);
 }
 
-fn print_src(message: &str, src: &ArrayViewMut2<Complex32>) {
-    let prettier = unsafe {
-        std::mem::transmute::<&ArrayViewMut2<Complex32>, &ArrayViewMut2<PrettierIndex>>(src)
-    };
-    println!("{message}\n{:#?}", prettier);
-}
-
-#[derive(bytemuck::Pod, Clone, Copy, Zeroable)]
-#[repr(transparent)]
-struct PrettierIndex([f32; 2]);
-impl std::fmt::Debug for PrettierIndex {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("({}, {})", self.0[0], self.0[1]))
-    }
-}
-
-fn pure_transpose_test<RT: Runtime>(client: &ComputeClient<RT::Server, RT::Channel>) {
-    let rows = 20;
-    let cols = 20;
-    let data =
-        ndarray::Array::from_shape_fn([rows, cols], |(i, j)| PrettierIndex([i as f32, j as f32]));
-
-    dbg!(&data);
-    let bytes_data = bytemuck::cast_slice::<_, u8>(data.as_slice().unwrap());
-    let in_handle = client.create(bytes_data);
-    let out_handle = client.empty(bytes_data.len());
-
-    let n_batches = 1;
-    let n_tile_rows = 40;
-    let n_tile_cols = 40;
-
-    let input_arg = unsafe { ArrayArg::<RT>::from_raw_parts::<f32>(&in_handle, rows * cols, 2) };
-    let output_arg = unsafe { ArrayArg::<RT>::from_raw_parts::<f32>(&out_handle, rows * cols, 2) };
-
-    let tile_side_len = 4;
-    let uniform = TwiddleTransposeUniformLaunch::new(
-        ScalarArg::new(rows as u32),
-        ScalarArg::new(cols as u32),
-        &tile_side_len,
-        ScalarArg::new(1),
-    );
-
-    unsafe {
-        twiddle_transpose2d_tiled::launch_unchecked(
-            client,
-            CubeCount::Static(n_batches, n_tile_rows, n_tile_cols),
-            CubeDim::new(256, 1, 1),
-            input_arg,
-            output_arg,
-            ScalarArg::new(-1.0),
-            uniform,
-        );
-    }
-
-    let out = bytemuck::cast_slice::<u8, PrettierIndex>(&client.read_one(out_handle)).to_vec();
-
-    let recovered = ndarray::Array::from_shape_vec([cols, rows], out).unwrap();
-    dbg!(&recovered);
-}
-
-fn correctness_tests<RT: Runtime>(
-    client: &ComputeClient<RT::Server, RT::Channel>,
-    full_data: Vec<f32>,
-    n: u32,
-    t: u32,
-) -> Vec<f32> {
-    let vec_width = 2;
-    let n_elem = full_data.len() as u32 / 2;
-    let handle = client.create(f32::as_bytes(&full_data));
-    let output_handle = client.empty(full_data.len() * std::mem::size_of::<f32>());
-    let d_input =
-        unsafe { ArrayArg::<RT>::from_raw_parts::<f32>(&handle, full_data.len(), vec_width as u8) };
-    let r = n / t;
-    let n_batches = n_elem as u32 / n;
-    let n_ffts = n_elem as u32 / t;
-    unsafe {
-        fft1d_r2_fused::launch_unchecked::<RT>(
-            &client,
-            CubeCount::Static(n_ffts, 1, 1),
-            CubeDim::new(256, 1, 1),
-            d_input,
-            ScalarArg::new(-1.),
-            t,
-        );
-    }
-
-    let n_cols = t;
-    let n_rows = r;
-
-    let tile_side_len = 64;
-
-    let n_tile_rows = n_rows.div_ceil(tile_side_len);
-    let n_tile_cols = n_cols.div_ceil(tile_side_len);
-
-    let input_arg = unsafe { ArrayArg::<RT>::from_raw_parts::<f32>(&handle, full_data.len(), 2) };
-    let output_arg =
-        unsafe { ArrayArg::<RT>::from_raw_parts::<f32>(&output_handle, full_data.len(), 2) };
-
-    let uniform = TwiddleTransposeUniformLaunch::new(
-        ScalarArg::new(n_rows),
-        ScalarArg::new(n_cols),
-        &tile_side_len,
-        ScalarArg::new(1),
-    );
-
-    unsafe {
-        twiddle_transpose2d_tiled::launch_unchecked(
-            client,
-            CubeCount::Static(n_batches, n_tile_rows, n_tile_cols),
-            CubeDim::new(256, 1, 1),
-            input_arg,
-            output_arg,
-            ScalarArg::new(-1.0),
-            uniform,
-        );
-    }
-
-    let n_ffts = n_elem / r;
-    let output_arg = unsafe {
-        ArrayArg::<RT>::from_raw_parts::<f32>(&output_handle, full_data.len(), vec_width as u8)
-    };
-    unsafe {
-        fft1d_r2_fused::launch_unchecked::<RT>(
-            &client,
-            CubeCount::Static(n_ffts, 1, 1),
-            CubeDim::new(256, 1, 1),
-            output_arg,
-            ScalarArg::new(-1.),
-            r,
-        );
-    }
-
-    let output = f32::from_bytes(&client.read_one(output_handle)).to_vec();
-
-    // let input_arg = unsafe { ArrayArg::<RT>::from_raw_parts::<f32>(&handle, full_data.len(), 2) };
-    // let output_arg =
-    //     unsafe { ArrayArg::<RT>::from_raw_parts::<f32>(&output_handle, full_data.len(), 2) };
-
-    // let uniform = TwiddleTransposeUniformLaunch::new(
-    //     ScalarArg::new(n_cols),
-    //     ScalarArg::new(n_rows),
-    //     &tile_side_len,
-    //     ScalarArg::new(0),
-    // );
-
-    // unsafe {
-    //     twiddle_transpose2d_tiled::launch_unchecked(
-    //         client,
-    //         CubeCount::Static(n_batches, n_tile_cols, n_tile_rows),
-    //         CubeDim::new(256, 1, 1),
-    //         output_arg,
-    //         input_arg,
-    //         ScalarArg::new(-1.0),
-    //         uniform,
-    //     );
-    // }
-
-    // let output = f32::from_bytes(&client.read_one(handle)).to_vec();
-
-    output
-}
-
-struct FftBenchmark<R: Runtime> {
-    client: ComputeClient<R::Server, R::Channel>,
-    plan: FftPlan,
-    full_data: Vec<f32>,
-}
-
-impl<R: Runtime> Benchmark for FftBenchmark<R> {
-    type Input = (Handle, Handle);
-
-    type Output = Handle;
-
-    fn prepare(&self) -> Self::Input {
-        let input = self.client.create(f32::as_bytes(&self.full_data));
-        let tmp = self.client.create(f32::as_bytes(&self.full_data));
-        (input, tmp)
-    }
-
-    fn execute(&self, input: Self::Input) -> Result<Self::Output, String> {
-        let (mut input, mut tmp) = input;
-        let output = execute_ops::<R>(
-            input,
-            &mut tmp,
-            &self.plan,
-            Direction::Forward,
-            &self.client,
-            &FftParams {
-                vec_width: 4,
-                local_side_len: 64,
-                threads_per_cube: 256,
-            },
-        );
-
-        Ok(output)
-    }
-
-    fn name(&self) -> String {
-        "Fused fft kernel in local memory".to_string()
-    }
-
-    fn sync(&self) {
-        future::block_on(self.client.sync())
-    }
-}
